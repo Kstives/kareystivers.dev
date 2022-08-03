586e3e722efd709e670b974c167bb6ab67f7a1cb
Kstives <58517350+Kstives@users.noreply.github.com>
2022-08-02 20:22:29 -0700

    adding index files

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Karey Stivers Portfolio Site</title>
+<meta name="Home page for Karey Stivers Portfolio Site" content="Home">
+<meta name="keywords" content="web design portfolio site">
+</head>
+<body>
+<div>
+  <h1>Karey Stivers</h1>
+  <h2>I am a designer and front-end developer with a passion for solving real-world problems in efficient, effective, and creative ways</h2>
+</div>
+
+<div id="highlight-reel">
+  <button id="highlight-reel-btn" class="link-btn" ><a href="">View More</a></button>
+</div>
+
+<div>
+  <ul>
+    <li><a href="web-design-and-dev/index.php">Web Design + Dev</a></li>
+    <li><a href="branding/index.php">Branding</a></li>
+    <li><a href="photography/index.php">Photography</a></li>
+  </ul>
+</div>
+<?php include '<footer.php';?>
+</body>
+</html>
