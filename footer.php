8e72946eb6189bc46f70968fccf3fa9da6ba930d
Kstives <58517350+Kstives@users.noreply.github.com>
2022-08-03 17:42:11 -0700

    changed files from php to html

--- footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-  <ul id="footer-contact">
-    <li>Karey Stivers</li>
-    <li>karey@kareystivers.dev</li>
-    <li><a href="tel:+2086996597">208-699-6597</a></li>
-    <li><a href="resume/index.php">Resume</a></li>
-  </ul>
-  <p>© Karey Stivers 2022</p> 
-</footer>
